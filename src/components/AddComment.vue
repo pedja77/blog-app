ef0c2e8f23758753f55e85c4cd85a89a2c35e82d
pedja77 <pedja77@stcable.net>
2018-05-11 20:56:08 +0200

    Zadatak 9 parcijalno

---
+++ src/components/AddComment.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container mt-8">
+    <form @submit.prevent="submitComment">
+    <div class="form-group row">
+      <label for="username" class="col-4 col-form-label">Username</label> 
+      <div class="col-8">
+        <input id="username" 
+          name="username" 
+          placeholder="Enter your username" 
+          type="text" 
+          class="form-control here"
+          required>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="text" class="col-4 col-form-label">Comment</label> 
+      <div class="col-8">
+        <textarea 
+          id="text" 
+          name="text" 
+          cols="40" 
+          rows="4" 
+          class="form-control"
+          required>
+        </textarea>
+      </div>
+    </div> 
+    <div class="form-group row">
+      <div class="offset-4 col-8">
+        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -2,13 +2,18 @@
   <div>
       <h3>{{ post.title }}</h3>
       <p>{{ post.text }}</p>
+      <add-comment></add-comment>
   </div>
 </template>
 
 <script>
 import { posts } from "../services/Posts";
+import AddComment from './AddComment.vue'
 
 export default {
+  components: {
+      AddComment
+  },
   data() {
     return {
       post: {}
